5f4fa34ab6f032cfc3e0c51783742aeab100071d
Anandh <anandh272004@gmail.com>
2024-08-09 19:56:20 +0530

    update

--- project/src/Home.jsx
+++ project/src/Home.jsx
@@ -1,8 +1,8 @@
-
 import React, { useState } from 'react';
 import { IoMdContact } from "react-icons/io";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { FaBars, FaTimes } from 'react-icons/fa';
+
 
 const Example = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,6 +10,8 @@ const Example = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
+  
 
   return (
     <nav className="flex justify-between items-center py-4 bg-teal-700 shadow-md">
@@ -19,6 +21,7 @@ const Example = () => {
         </a>
       </div>
       {/* Toggle button */}
+     
       <div className="md:hidden mr-6 lg:mr-9" onClick={toggleMenu}>
         {isOpen ? (
           <FaTimes className="text-neutral-50 text-2xl cursor-pointer hover:text-amber-500 transition-all duration-300 ease-in-out" />
@@ -27,40 +30,41 @@ const Example = () => {
         )}
       </div>
       {/* Navigation links */}
-      <ul className={`md:flex space-y-4 md:space-y-0 space-x-0 md:space-x-6 lg:space-x-12 justify-between items-center pt-4 md:pt-0 mr-6 lg:mr-12 ${isOpen ? 'block' : 'hidden'} md:block`}>
+     
+      <ul  className={`md:flex space-y-4 md:space-y-0 space-x-0 md:space-x-6 lg:space-x-12 justify-between items-center pt-4 md:pt-0 mr-6 lg:mr-12 ${isOpen ? 'block' : 'hidden'} md:block`}>
         {/* Dummy */}
         <li className="mr-4 lg:mr-6">
-          <a href="#" className="text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
+          <a href="/" className="text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
             {/* Placeholder */}
           </a>
         </li>
         <li className="mr-4 lg:mr-6">
-          <a href="#" className="transition-all duration-300 ease-in-out text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
+          <a href="/" className="transition-all duration-300 ease-in-out text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
             Home
           </a>
         </li>
         <li className="mr-4 lg:mr-6">
-          <a href="#" className="transition-all duration-300 ease-in-out text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
+          <a href="/shop" className="transition-all duration-300 ease-in-out text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
             Shop
           </a>
         </li>
         <li className="mr-4 lg:mr-6">
-          <a href="#" className="transition-all duration-300 ease-in-out text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
+          <a href="/about-us" className="transition-all duration-300 ease-in-out text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
             About Us
           </a>
         </li>
         <li className="mr-4 lg:mr-6">
-          <a href="#" className="transition-all duration-300 ease-in-out text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
+          <a href="/services" className="transition-all duration-300 ease-in-out text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
             Services
           </a>
         </li>
         <li className="mr-4 lg:mr-6">
-          <a href="#" className="transition-all duration-300 ease-in-out text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
+          <a href="/blog" className="transition-all duration-300 ease-in-out text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
             Blog
           </a>
         </li>
         <li className="mr-4 lg:mr-6">
-          <a href="#" className="transition-all duration-300 ease-in-out text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
+          <a href="/contact-us" className="transition-all duration-300 ease-in-out text-gray-400 no-underline hover:text-neutral-50 font-semibold hover:underline decoration-2 hover:decoration-amber-500">
             Contact Us
           </a>
         </li>
@@ -75,6 +79,7 @@ const Example = () => {
           </a>
         </li>
       </ul>
+     
     </nav>
   );
 };
