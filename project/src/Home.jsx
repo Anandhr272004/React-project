10e44bbde7d5e522ca0a72791a101624b89a6150
Anandh <anandh272004@gmail.com>
2024-08-21 18:49:27 +0530

    update

--- project/src/Home.jsx
+++ project/src/Home.jsx
@@ -5,38 +5,68 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import data from './Navbar.json';
 import { IoIosContact } from "react-icons/io";
 import { CiShoppingCart } from "react-icons/ci";
+import sectionone from"./Sectionone.json";
 
 function NavBar() {
   return (
-    <div className='one'>
-      <Navbar expand="lg"  >
-        <Container  >
-      
-          <Navbar.Brand href="/" id='furni'>Furni.</Navbar.Brand>
-       
-            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-            {/* <div id='sec'> */}
+    <div className='Home'>
+      <div className='nave'>
+        <Navbar expand="lg"  >
+          <Container>
+            {/* <Row>
+            <Col> */}
+            <Navbar.Brand href="/" id='furni'>Furni.</Navbar.Brand>
+            {/* </Col>
+            <Col> */}
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-      
               <Nav className="me-auto">
-              
                 {data.map((item) => (
-                  <Nav.Link key={item.id} id='name'>{item.name}</Nav.Link>
+                  <span className='sec'>
+                    <Nav.Link key={item.id} id='name'>{item.name}</Nav.Link>
+                  </span>
                 ))}
-              
                 <Nav.Link id='contactone'><IoIosContact /></Nav.Link>
                 <Nav.Link id='contactone'><CiShoppingCart /></Nav.Link>
-                
               </Nav>
             </Navbar.Collapse>
-          {/* </div> */}
+            {/* </Col>
+          </Row> */}
+          </Container>
+        </Navbar>
+      </div>
+
+      <div className='sectionone'>
+        <Container>
+          <Row>
+            <Col lg={4} className='sectiona'>
+            {sectionone.map((sectionone=>(
+              <p key={sectionone.id}>{sectionone.name}</p>
+            )))}
+            
+            </Col>
+            <Col lg={8} className='sectionb'>
+            </Col>
+          </Row>
+
         </Container>
-      </Navbar>
+
+      </div>
+
+
+
+
+
+
+
+
     </div>
+
+
+
 
 
   );
 }
 
 export default NavBar;
-
